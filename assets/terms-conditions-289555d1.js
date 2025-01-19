627c7f8c178891714426953ff7892bdb073a14c8
Yarzik7 <116737021+Yarzik7@users.noreply.github.com>
2025-01-19 17:19:59 +0000

    Deploying to gh-pages from @ artem-yaremchuk/evoplay@7a01f1820e179b46d2ddda30b755ac2e55e7589e 🚀

--- assets/terms-conditions-289555d1.js
+++ assets/terms-conditions-289555d1.js
@@ -1,2 +1,2 @@
 const a=document.querySelectorAll(".js-legal-nav-link");a.forEach(e=>{e.href===window.location.href&&(e.classList.add("current"),e.setAttribute("aria-current","page"))});
-//# sourceMappingURL=terms-conditions-09a3b294.js.map
+//# sourceMappingURL=terms-conditions-289555d1.js.map
