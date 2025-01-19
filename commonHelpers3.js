a57387407d74935acc4dbf4f144fc8cb7622cd7d
Yarzik7 <116737021+Yarzik7@users.noreply.github.com>
2025-01-19 08:15:45 +0000

    Deploying to gh-pages from @ artem-yaremchuk/evoplay@b3e7321fa78182be74e26cb9cdc9519d9bc206ee 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/styles-822b9d3e.js";const e=document.querySelectorAll(".js-legal-nav-link");e.forEach(t=>{t.href===window.location.href&&(t.classList.add("current"),t.setAttribute("aria-current","page"))});
+//# sourceMappingURL=commonHelpers3.js.map
