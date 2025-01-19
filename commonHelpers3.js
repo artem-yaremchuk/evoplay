e62201599ca0f1fa4c65e91f9b38eb20cc179a3a
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-01-19 10:12:55 +0000

    Deploying to gh-pages from @ artem-yaremchuk/evoplay@7776480ce78c474f1856c3f77ae49ad718f05e8f 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,2 @@
-import"./assets/styles-822b9d3e.js";const e=document.querySelectorAll(".js-legal-nav-link");e.forEach(t=>{t.href===window.location.href&&(t.classList.add("current"),t.setAttribute("aria-current","page"))});
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const e=document.querySelectorAll(".js-legal-nav-link");e.forEach(t=>{t.href===window.location.href&&(t.classList.add("current"),t.setAttribute("aria-current","page"))});
 //# sourceMappingURL=commonHelpers3.js.map
