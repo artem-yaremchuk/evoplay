f7377a9be7600b89b552e000544b95cc65a63209
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2025-01-22 22:24:03 +0000

    Deploying to gh-pages from @ artem-yaremchuk/evoplay@aa85258bcda95c322c4fc0e9e39bbb1b4da940d5 🚀

--- assets/terms-conditions-344c642d.js
+++ assets/terms-conditions-344c642d.js
@@ -1,2 +1,2 @@
 const a=document.querySelectorAll(".js-legal-nav-link");a.forEach(e=>{e.href===window.location.href&&(e.classList.add("current"),e.setAttribute("aria-current","page"))});
-//# sourceMappingURL=terms-conditions-289555d1.js.map
+//# sourceMappingURL=terms-conditions-344c642d.js.map
