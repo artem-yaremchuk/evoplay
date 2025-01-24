17ef88a452562cf43981471ba1bf9fa5903033b0
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2025-01-24 02:55:09 +0200

    fix(reviews slider): fix reviews slider behavior when responsive

--- src/js/reviewsSlider.js
+++ src/js/reviewsSlider.js
@@ -1,11 +1,7 @@
 import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
 
 new Swiper('.reviews-swiper', {
   loop: true,
-  grid: {
-    rows: 2,
-  },
   navigation: {
     nextEl: '.reviews-slider-button-next',
     prevEl: '.reviews-slider-button-prev',
@@ -14,10 +10,16 @@ new Swiper('.reviews-swiper', {
     100: {
       slidesPerView: 1,
       spaceBetween: 16,
+      grid: {
+        rows: 2,
+      },
     },
     1200: {
       slidesPerView: 2,
       spaceBetween: 24,
+      grid: {
+        rows: 2,
+      },
     },
   },
 });
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import 'swiper/css/bundle';
 import './js/mobile-menu';
 import './js/gallerySlider';
 import './js/onShowFAQAnswer';
